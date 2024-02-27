e080c2757d279688d771c01e931bd628178d5307
Christopher <christopher.wilke86@googlemail.com>
2024-02-27 13:10:20 +0100

    Layer of Neurons

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+struct NeuronsLayer {
+    inputs: [f64; 4],
+    weights: [[f64; 4]; 3],
+    bias: [f64; 3],
+}
+
+impl NeuronsLayer {
+    pub fn forward(&mut self) {
+        let mut value = 0.;
+        for neuron in self.weights {
+            let n_val = 0.;
+            for (idx, val) in neuron.iter().enumerate() {
+                println!("{}, {}", idx, val);
+            }
+            println!("---");
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut layer = NeuronsLayer {
+        inputs: [1., 2., 3., 2.5],
+        weights: [
+            [0.2, 0.8, -0.5, 1.0],
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, -0.27, 0.17, 0.87],
+        ],
+        bias: [1.0, 2.0, 0.4],
+    };
+    layer.forward();
 }
