1c88ee1051a2126ee0ed2acf746e7d5105aa0451
Christopher <christopher.wilke86@googlemail.com>
2024-03-13 16:22:01 +0100

    transpose testing

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,31 @@ use fynn_array::FynnArray;
 use neurons_layer::NeuronsLayer;
 
 pub fn main() {
+
+    let test_array = FynnArray::new(&[
+        [0, 1, 2, 3, 4],
+        [5, 6, 7, 8, 9],
+        [10, 11, 12, 13, 14],
+        [15, 16, 17, 18, 19]
+    ]);
+
+    println!("{:?}", test_array.transpose());
  
-    let inputs = [1., 2., 3., 2.5];
-    let weights = [
-        [0.2, 0.8, -0.5, 1.],
-        [0.5, -0.91, 0.26, -0.5],
-        [-0.26, -0.27, 0.17, 0.87]
-    ];
-    let bias = [2., 3., 0.5];
+    // let inputs = [1., 2., 3., 2.5];
+    // let weights = [
+    //     [0.2, 0.8, -0.5, 1.],
+    //     [0.5, -0.91, 0.26, -0.5],
+    //     [-0.26, -0.27, 0.17, 0.87]
+    // ];
+    // let bias = [2., 3., 0.5];
 
-    let mut layer = NeuronsLayer {
-        inputs: inputs,
-        weights: weights,
-        bias: bias,
-        output: Vec::new()
-    };
+    // let mut layer = NeuronsLayer {
+    //     inputs: inputs,
+    //     weights: weights,
+    //     bias: bias,
+    //     output: Vec::new()
+    // };
 
-    layer.forward();
-    println!("layer output = {:?}", layer.output);
+    // layer.forward();
+    // println!("layer output = {:?}", layer.output);
 }
