479903287f731e6fb53cfbe058deb21349740e1f
Christopher <christopher.wilke86@googlemail.com>
2024-03-13 16:13:49 +0100

    Simple NeuronsLayer

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,26 @@
-// use std::ops::{Add, Sub};
-
-// use crate::fynn_array::FynnArray;
+mod fynn_array;
+mod neurons_layer;
 
 use fynn_array::FynnArray;
+use neurons_layer::NeuronsLayer;
 
-mod fynn_array;
-// mod helpers;
+pub fn main() {
+ 
+    let inputs = [1., 2., 3., 2.5];
+    let weights = [
+        [0.2, 0.8, -0.5, 1.],
+        [0.5, -0.91, 0.26, -0.5],
+        [-0.26, -0.27, 0.17, 0.87]
+    ];
+    let bias = [2., 3., 0.5];
 
-struct NeuronsLayer {
-    inputs: [f64; 4],
-    weights: [[f64; 4]; 3],
-    bias: [f64; 3],
-    output: Vec<f64>,
-}
+    let mut layer = NeuronsLayer {
+        inputs: inputs,
+        weights: weights,
+        bias: bias,
+        output: Vec::new()
+    };
 
-impl NeuronsLayer {
-    pub fn forward(&mut self) {
-        for (i, bias) in self.bias.iter().enumerate() {
-            let mut n_val = 0.;
-            for (j, input) in self.inputs.iter().enumerate() {
-                n_val += input * self.weights[i][j];
-            }
-            n_val += bias;
-            self.output.push(n_val);
-        }
-    }
-
-    pub fn get_output(&self) -> &Vec<f64> {
-        &self.output
-    }
-}
-
-fn main() {
-    let array_2d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-    let mut fynn_array = FynnArray::new(&array_2d);
-
-    println!("Before:");
-    println!("{:?}", fynn_array);
-    
-    fynn_array = fynn_array.transpose();
-
-    println!("transposed:");
-    println!("{:?}", fynn_array);
-
+    layer.forward();
+    println!("layer output = {:?}", layer.output);
 }
