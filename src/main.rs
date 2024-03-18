0d4944dba7add04f8832aa478ecb7e58e2eb4de8
Christopher <christopher.wilke86@googlemail.com>
2024-03-18 12:46:00 +0100

    2 hidden layers

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,20 @@ pub fn main() {
         [0.5, -0.91, 0.26, -0.5],
         [-0.26, -0.27, 0.17, 0.87]
     ]
-    .to_fynn_array()
-    .transpose();
-
+    .to_fynn_array();
     let bias = [2.0, 3.0, 0.5];
-    let out = helpers::dot(&inputs, &weights);
-    let o_add_op = out + &bias;
 
-    println!("{o_add_op:?}");
+    let weights2 = [
+        [0.1, -0.14, 0.5],
+        [-0.5, 0.12, -0.33],
+        [-0.44, 0.73, -0.13]
+    ]
+    .to_fynn_array();
+    let bias2 = [-1., 2., -0.5];
+    
+    let layer1_outputs = helpers::dot(&inputs, &weights.transpose()) + &bias;
+    let layer2_outputs = helpers::dot(&layer1_outputs, &weights2.transpose()) + &bias2;
+
+    println!("{layer2_outputs:?}");
 
 }
