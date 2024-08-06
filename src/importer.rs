aba9537b1bcfb4333ac1bb8cf53435d457176ccb
Christopher <christopher.wilke86@googlemail.com>
2024-08-06 15:25:28 +0200

    wip: fixing importer

--- src/importer.rs
+++ src/importer.rs
@@ -1,6 +1,5 @@
 use std::{
-    fs::File,
-    io::{self, BufRead},
+    any::Any, fmt::Debug, fs::File, io::{self, BufRead, BufReader}
 };
 
 use crate::{FynnArray, FynnBehavior};
@@ -11,6 +10,11 @@ pub struct Point {
     pub Y: f64,
 }
 
+pub enum InputType {
+    Integer(Vec<i32>),
+    Float(Vec<f64>)
+}
+
 #[derive(Clone)]
 pub struct Importer {
     pub v: Vec<Point>,
@@ -19,35 +23,73 @@ pub struct Importer {
 
 impl Importer {
 
-    pub fn from_files(input: &str, y_true: &str) -> (FynnArray, Vec<i32>) {
+    pub fn from_files(input: &str, y_true: &str) -> (FynnArray, InputType) {
         log::trace!("Input: {input}, y_true: {y_true}");
 
-        // Input
+        // // Input
         let f_input = File::open(input).expect("could not open file");
         let input_val = Self::to_vec(f_input);
-        let input_out = Self::input_converter(input_val);
+        // let input_out = Self::input_converter(input_val);
 
         // Y_true
         let f_y_true = File::open(y_true).expect("could not open file");
-        let y_true_val:  = Self::to_vec::<Vec<i32>>(f_y_true);
+        let mut y_true_val = Self::to_vec(f_y_true);
 
-        (input_out, y_true_val)
+        (FynnArray::new(), y_true_val)
     }
 
-    pub fn to_vec<T: std::str::FromStr + Default + std::fmt::Debug>(input: File) -> Vec<T> {
-        let reader = io::BufReader::new(input).lines();
-        let mut resp = vec![];
+    pub fn to_vec(input: File) -> InputType {
+        let first_line = io::BufReader::new(&input)
+            .lines()
+            .next();
 
-        for line in reader.flatten() {
-            for v in line.split_whitespace() {
-                log::info!("v={}", v);
-                let val = v.parse::<T>().expect("some error");
-                // log::info!("converted={:?}", val);
-                resp.push(val);
-            }
+        if let Some(val) = first_line 
+        {
+            let zeros = val
+                .unwrap()
+                .chars()
+                .filter(|c| c == &'0')
+                .count();
+
+            let reader = io::BufReader::new(input);
+
+            match zeros {
+                21 => Self::get_y(),
+                _ => log::info!("I am X")
+            }    
+        };
+        
+        InputType::Integer(vec![])
+    }
+
+    fn get_y() {
+        log::info!("executing get_y");
+        let f_y_true = File::open("py/out_Y.txt").expect("could not open file");
+        let reader = io::BufReader::new(f_y_true);
+
+        for line in reader.lines() {
+            log::info!("{}", line.unwrap());
         }
-        resp    
+    
+        // for line in reader.lines() {
+        //     let val = line.unwrap();
+        //     log::info!("jo");
+        // }
     }
+        
+        // for line in reader.flatten() {
+        //     for v in line.split_whitespace() {
+        //         // log::info!("v={}", v);
+        //         let num = "0".to_owned();
+        //         let val = match v.parse::<T>() {
+        //             Ok(v) => v,
+        //             Err(e) => { panic!("panic!!! {e:?}") }
+        //         };
+        //         resp.push(val);
+        //     }
+        // }
+        // resp    
+    // }
 
     fn input_converter(values: Vec<f64>) -> FynnArray {
         let mut v: Vec<Point> = vec![];
@@ -62,3 +104,5 @@ impl Importer {
     }
 
 }
+
+
